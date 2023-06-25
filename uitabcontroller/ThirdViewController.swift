9a18333ebcd9c85522700d6ca93f701cc84e0320
MilanaAstamirova <135124731+MilanaAstamirova@users.noreply.github.com>
2023-06-25 18:02:25 +0500

    UiTabBarController

---
+++ uitabcontroller/ThirdViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ThirdViewController.swift
+//  uitabcontroller
+//
+//  Created by doschitai on 25.06.2023.
+//
+
+import Foundation
---
+++ uitabcontroller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uitabcontroller
+//
+//  Created by doschitai on 25.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
